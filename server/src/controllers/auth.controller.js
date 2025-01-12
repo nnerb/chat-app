fede03d68c9c5160edc8b968f983a2d089dee199
Brenn Santiago <brennaldwin.santiago@gmail.com>
2025-01-12 18:37:10 +0800

    update imports and finish update profile endpoint

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js"
 import { generateToken } from "../lib/utils.js"
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
@@ -92,6 +93,30 @@ export const logout = (req, res) => {
    });
   } catch (error) {
     console.log("Error in logout controller", error)
+    res.status(500).json({ success: false, message: "Internal server error" })
+  }
+}
+
+export const updateProfile = async(req, res) => {
+  try {
+    const { profilePic } = req.body
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.send(400).json({ success: false, message: "Profile picture is required" })
+    }
+
+   const uploadResponse = await cloudinary.uploader.upload(profilePic)
+   const updatedUser = User.findByIdAndUpdate(
+    userId, 
+    { profilePic: uploadResponse.secure_url},
+    { new: true }
+  )
+
+   return res.status(200).json({ success: true, user: updatedUser})
+
+  } catch (error) {
+    console.log("Error in update profile controller", error)
     res.status(500).json({ success: false, message: "Internal server error" })
   }
 }
--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model';
+import User from '../models/user.model.js';
 
 export default async function protectRoute(req, res, next) {
   try {
