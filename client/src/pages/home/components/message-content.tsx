0ff4b24649dc35e90f1e3523de649da8c5c0346d
Brenn Santiago <brennaldwin.santiago@gmail.com>
2025-03-16 15:22:14 +0800

    refactor: modularize TypingIndicator component

--- client/src/pages/home/components/message-content.tsx
+++ client/src/pages/home/components/message-content.tsx
@@ -4,6 +4,7 @@ import IndividualChat from "./individual-chat";
 import { useCallback, useEffect, useRef } from "react";
 import { useAuthStore } from "../../../store/useAuthStore";
 import { useParams } from "react-router-dom";
+import TypingIndicator from "./typing-indicator";
 
 const MessageContent = () => {
   const { 
@@ -70,24 +71,7 @@ const MessageContent = () => {
         </div>
       ))}
       {selectedUser && typingUsers.includes(selectedUser?._id) && (
-        <div className="flex gap-1.5">
-          <div className="chat-image avatar">
-            <div className="size-10 rounded-full border flex">
-              <img
-                src={selectedUser?.profilePic || "/avatar.png"}
-                alt="profile pic"
-              />
-            </div>
-          </div>
-           {/* Typing indicator dots */}
-           <div className="space-x-1 rounded-2xl p-1 grid place-items-center">
-              <div className="flex items-center space-x-1 bg-base-200 p-3 rounded-2xl animate-pulse duration-1000">
-                <div className="h-1 w-1 bg-gray-400 rounded-full animate-bounce delay-100" />
-                <div className="h-1 w-1 bg-gray-400 rounded-full animate-bounce delay-200" />
-                <div className="h-1 w-1 bg-gray-400 rounded-full animate-bounce delay-300" />
-              </div>
-            </div>
-        </div>
+        <TypingIndicator selectedUser={selectedUser} />
       )}
     </div>
    );
